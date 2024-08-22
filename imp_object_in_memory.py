dc3199b1e7449895e2a1cdd52e5a67bd3afc9021
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-08-22 08:55:44 +0530

    Add files via upload

--- imp_object_in_memory.py
+++ imp_object_in_memory.py
@@ -1,14 +1,16 @@
-# def make_ordered_list(n):
-#     """ n is a positive int
-#     Returns a list containing all ints in order 
-#     from 0 to n (inclusive)
-#     """
-#     L = []
-#     for i in range(n+1):
-#         L.append(i)
-#     return L
+#### 21st and 22nd August, MIT Ana Bell 10
 
-# print(make_ordered_list(6))  # prints [0, 1, 2, 3, 4, 5, 6]
+def make_ordered_list(n):
+    """ n is a positive int
+    Returns a list containing all ints in order 
+    from 0 to n (inclusive)
+    """
+    L = []
+    for i in range(n+1):
+        L.append(i)
+    return L
+
+print(make_ordered_list(6))  # prints [0, 1, 2, 3, 4, 5, 6]
 
 
 # Question 2
@@ -28,4 +30,38 @@ print(id(brunch), " id")
 
 brunch.extend(L2)
 print("brunch ",brunch)
-# What's the value of brunch here?   
+# What's the value of brunch here?  ['bacon', 'eggs', 'juice', 'toast', 'jam'] 
+
+# brunch  ['bacon', 'eggs']
+# 4303533056  id
+# L1  ['bacon', 'eggs', 'juice']
+# 4303533056  id
+# brunch  ['bacon', 'eggs', 'juice']
+# 4303533056  id
+# brunch  ['bacon', 'eggs', 'juice', 'toast', 'jam'] 
+
+
+
+
+#######################################
+############ AT HOME ###############
+#######################################
+## Question 3. 
+def apply_to_each(L, f):
+    """ L is a list of numbers 
+        f is a list that takes in a number and returns a number
+    Mutate L such that you apply function f to every element in L """
+    # your code here
+    for i in range(len(L)):
+        L[i] = f(L[i])
+    return L
+
+
+
+test = [1,-2,3]
+apply_to_each(test, lambda x: x**2)
+print(test)   # prints [1,4,9]
+
+test = [-7, 8, 5, -8, -3]
+apply_to_each(test, abs)
+print(test)   # prints [7, 8, 5, 8, 3]
