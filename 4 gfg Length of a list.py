e016e9da5d325b54a559a43a6e2e16e750e30d9b
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-08-25 18:04:27 +0530

    Add files via upload

--- 4 gfg Length of a list.py
+++ 4 gfg Length of a list.py
@@ -1,84 +1,84 @@
-################    25th Aug 
+###############    25th Aug 
 
 
-######## My solution - 1   ## wrong
+####### My solution - 1   ## wrong
 
-# def len_list(*args):
-#     for lists_ in args:
-#         return len(lists_)
+def len_list(*args):
+    for lists_ in args:
+        return len(lists_)
     
 
-# L = list(input("Enter the list : "))
+L = list(input("Enter the list : "))
 
-# print(len_list())
+print(len_list())
 
-# # Enter the list : [1,2,3] [4,5,6] [7,2,1,5,3]
-# # None
+# Enter the list : [1,2,3] [4,5,6] [7,2,1,5,3]
+# None
     
  
-# ##############  Solution --- Chatgpt 
+##############  Solution --- Chatgpt 
 
-# def len_list(*args):
-#     lengths = []
-#     for lists_ in args:
-#         lengths.append(len(lists_))
-#     return lengths
+def len_list(*args):
+    lengths = []
+    for lists_ in args:
+        lengths.append(len(lists_))
+    return lengths
 
-# L1 = [1, 2, 3]
-# L2 = [4, 5, 6, 7]
-# L3 = [8, 9]
+L1 = [1, 2, 3]
+L2 = [4, 5, 6, 7]
+L3 = [8, 9]
 
-# print(len_list(L1, L2, L3))
-
-
-# #################  Different versions 
-
-# def len_list(*args):
-#     lengths = []
-#     for lists_ in args:
-#         lengths.append(len(lists_))
-#     return lengths
-
-# # Taking input for multiple lists
-# L1 = list(map(int, input("Enter the elements of the first list separated by space: ").split()))
-# L2 = list(map(int, input("Enter the elements of the second list separated by space: ").split()))
-# L3 = list(map(int, input("Enter the elements of the third list separated by space: ").split()))
-
-# # Calculating and printing the lengths of the lists
-# print("Lengths of the lists:", len_list(L1, L2, L3))
-
-# # Enter the elements of the first list separated by space: 1 2 3 4
-# # Enter the elements of the second list separated by space: 4 5 6 7
-# # Enter the elements of the third list separated by space: 6 3 42 98 32
-# # Lengths of the lists: [4, 4, 5]
+print(len_list(L1, L2, L3))
 
 
+#################  Different versions 
+
+def len_list(*args):
+    lengths = []
+    for lists_ in args:
+        lengths.append(len(lists_))
+    return lengths
+
+# Taking input for multiple lists
+L1 = list(map(int, input("Enter the elements of the first list separated by space: ").split()))
+L2 = list(map(int, input("Enter the elements of the second list separated by space: ").split()))
+L3 = list(map(int, input("Enter the elements of the third list separated by space: ").split()))
+
+# Calculating and printing the lengths of the lists
+print("Lengths of the lists:", len_list(L1, L2, L3))
+
+# Enter the elements of the first list separated by space: 1 2 3 4
+# Enter the elements of the second list separated by space: 4 5 6 7
+# Enter the elements of the third list separated by space: 6 3 42 98 32
+# Lengths of the lists: [4, 4, 5]
 
 
-#####################   Different Version 
 
 
-# def len_list(*args):
-#     lengths = []
-#     for lists_ in args:
-#         lengths.append(len(lists_))
-#     return lengths
+####################   Different Version 
 
-# # Taking input for multiple lists in one variable
-# input_data = input("Enter multiple lists separated by semicolons (e.g., 1 2 3; 4 5 6 7; 8 9): ")
 
-# # Splitting the input data into separate lists
-# lists = [list(map(int, lst.split())) for lst in input_data.split(';')]
+def len_list(*args):
+    lengths = []
+    for lists_ in args:
+        lengths.append(len(lists_))
+    return lengths
 
-# # Calculating and printing the lengths of the lists
-# print("Lengths of the lists:", len_list(*lists))
+# Taking input for multiple lists in one variable
+input_data = input("Enter multiple lists separated by semicolons (e.g., 1 2 3; 4 5 6 7; 8 9): ")
 
-# # lst = (1,2,3)
-# # lists = list(map(int, lst.split()))
-# # print(lists)
+# Splitting the input data into separate lists
+lists = [list(map(int, lst.split())) for lst in input_data.split(';')]
 
-# # Enter multiple lists separated by semicolons (e.g., 1 2 3; 4 5 6 7; 8 9): 1 2 3; 4 5 6 7; 8 9
-# # Lengths of the lists: [3, 4, 2]
+# Calculating and printing the lengths of the lists
+print("Lengths of the lists:", len_list(*lists))
+
+# lst = (1,2,3)
+# lists = list(map(int, lst.split()))
+# print(lists)
+
+# Enter multiple lists separated by semicolons (e.g., 1 2 3; 4 5 6 7; 8 9): 1 2 3; 4 5 6 7; 8 9
+# Lengths of the lists: [3, 4, 2]
 
 
 
