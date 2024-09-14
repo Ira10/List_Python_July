8af1b18da8a3287d9442e64111a8fac5b62383a3
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-09-14 16:34:15 +0530

    Add files via upload

---
+++ krish 5 remove duplicates from a list.py
@@ -0,0 +1,21 @@
+### 14th sep   my sol
+
+def duplicate_removal():      ### 3 mins
+    L = [1,2,3,4,6,5,1,2,2,8,3,9,10,11,5,6]
+    L_new = []
+
+    for i in L:
+        if i not in L_new:
+            L_new.append(i)
+    
+    return L_new
+
+print(duplicate_removal())  # [1, 2, 3, 4, 6, 5, 8, 9, 10, 11]
+
+### krish sol
+
+def remove_duplicates(lst):
+    return list(set(lst))
+
+print(remove_duplicates([1,2,3,4,6,5,1,2,2,8,3,9,10,11,5,6]))  # [1, 2, 3, 4, 6, 5, 8, 9, 10, 11]
+
