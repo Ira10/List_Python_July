e5d869825a2bc17eb8457af142e5b05ce38f332b
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-09-24 21:15:08 +0530

    Add files via upload

--- 6 gfg Check if element exists in list.py
+++ 6 gfg Check if element exists in list.py
@@ -1,60 +1,70 @@
-# ############ 19th Sep
+############ 19th Sep
 
-# #### my sol ### 3 mins
+#### my sol ### 3 mins
 
-# L = [1, 6, 3, 5, 3, 4]
+L = [1, 6, 3, 5, 3, 4]
 
-# find_ = 3
+find_ = 3
 
-# for i in range(len(L)):
-#     if L[i] == find_:
-#         print("True")
-# #True
-# # True
+for i in range(len(L)):
+    if L[i] == find_:
+        print("True")
+# True
+# True
         
 
 
-# for i in range(len(L)):
-#     if L[i] == find_:
-#         print("True")
-#         break
+for i in range(len(L)):
+    if L[i] == find_:
+        print("True")
+        break
 
-# # True
+# True
     
 
-# ### 1. 'IN' operator
+### 1. 'IN' operator
     
-# L = [1, 6, 3, 5, 3, 4]
+L = [1, 6, 3, 5, 3, 4]
 
-# find_ = 3
+find_ = 3
 
-# if find_ in L:
-#     print("True")  
+if find_ in L:
+    print("True")  
 
 
-# ### 2. any() function
+### 2. any() function
     
-# # Initializing list
-# test_list = [1, 6, 3, 5, 3, 4]
+# Initializing list
+test_list = [1, 6, 3, 5, 3, 4]
 
-# result = any(item in test_list for item in test_list)
-# print("Does string contain any list element : " +str(bool(result)))
-# print(result)
+result = any(item in test_list for item in test_list)
+print("Does string contain any list element : " +str(bool(result)))
+print(result)
 
-# # Does string contain any list element : True
-# # True
+# Does string contain any list element : True
+# True
 
 
-# ########  3. count() function
+########  3. count() function
 
-# test_list = [1, 6, 3, 5, 3, 4]
+test_list = [1, 6, 3, 5, 3, 4]
 
-# count_ = test_list.count(3)
+count_ = test_list.count(3)
 
-# if count_ >=  1:
-#     print("exists")
+if count_ >=  1:
+    print("exists")
 
-# # exists
+# exists
 
 
 ########  4. count() function
+# Double all numbers using map and lambda
+
+numbers = (1, 2, 3, 4)
+# result = map(lambda x: x + x, numbers)
+result = (lambda x: x + x, numbers)
+print(list(result))
+
+y = lambda x : x + x 
+print(y(2))
+print(y(numbers))
