0216845fe7f3ac4c0980975326dd654fa5b458ef
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-09-30 20:57:25 +0530

    Add files via upload

--- 10 gfg Count occurrences in a list.py
+++ 10 gfg Count occurrences in a list.py
@@ -29,3 +29,12 @@ for i in char_list:
     if i == what_to_find_char:
         count_+=1
 print(count_)
+
+
+#######   3 . Using List Comprehension
+cnt = 0
+# count__ = [cnt + 1 for i in L if i == 3]  
+count__ = [1 for i in L if i == 3]  ### it's okay even if I don't do cnt+1
+print(len(count__))  ##### len is very very important
+print(count__)  ### [1, 1, 1, 1, 1, 1]
+
